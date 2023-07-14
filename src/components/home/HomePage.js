5646651fa4ef583779e2a32a7efa16ae3b66f580
HadiKia <hadikiamarzi@protonmain.com>
2023-07-15 00:12:42 +0330

    authors card created

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -6,17 +6,26 @@ import Blogs from "../blog/Blogs";
 const HomePage = () => {
   return (
     <Container maxWidth="lg">
-      <Grid container mt={12} sx={{paddingX : '.5em' , '@media screen and (min-width: 768px)': {
-        paddingX : '1.5em'
-        },}}>
-        <Grid item xs={12} sm={3} lg={2} mt={4}>
-          <Typography component="h3" variant="h5" mb={3} fontWeight={600}>
+      <Grid
+        container
+        mt={9}
+        sx={{
+          paddingX: ".5em",
+          "@media screen and (min-width: 768px)": {
+            paddingX: "1.5em",
+          },
+        }}
+      >
+        <Grid item xs={12} sm={3.5} lg={2.5} mt={4} sx={{"@media screen and (min-width: 768px)": {
+            paddingLeft: "1.25em",
+          },}}>
+          <Typography component="h3" variant="h5" mb={3} mr={2} fontWeight={600} sx={{color : '#5e6a86'}}>
             نویسنده ها
           </Typography>
           <Authors />
         </Grid>
-        <Grid item xs={12} sm={9} lg={10} mt={4}>
-          <Typography component="h3" variant="h5" mb={3} fontWeight={600}>
+        <Grid item xs={12} sm={8.5} lg={9.5} mt={4}>
+          <Typography component="h3" variant="h5" mb={3} mr={2} fontWeight={600} sx={{color : '#5e6a86'}}>
             مقالات
           </Typography>
           <Blogs />
