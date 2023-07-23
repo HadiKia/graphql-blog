2cc4ec184d1f7806f195282a3242b0eb99596683
HadiKia <hadikiamarzi@protonmain.com>
2023-07-23 22:36:09 +0330

    fix bugs

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import HomePage from "./components/home/HomePage";
 import Layout from "./components/layout";
 import BlogPage from "./components/blog/BlogPage";
 import AuthorPage from "./components/author/AuthorPage";
+import ScrollToTop from "./components/shared/ScrollToTop";
 
 function App() {
   return (
     <Layout>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/blogs/:slug" element={<BlogPage />} />
